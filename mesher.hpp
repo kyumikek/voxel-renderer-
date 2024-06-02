b54133e7e1046c28f231845d64d0069f71f4e9ea
kyubek <69163151+kyumikek@users.noreply.github.com>
2024-06-02 19:35:58 +0200

    Add files via upload
    infinite worlds, pog, greedy meshing modified pog

---
+++ mesher.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+#include "raylib.h"
+
+enum BLOCK_TYPES {
+    GRASS,
+    WATER,
+    STONE,
+    SAND,
+    DIRT,
+};
+struct BlockData {
+    uint8_t x;
+    uint8_t y;
+    uint8_t z;
+    uint8_t width;
+    uint8_t length;
+    uint8_t height;
+    bool obstructed;
+    bool frontObscured;
+    bool backObscured;
+    bool leftObscured;
+    bool rightObscured;
+    bool topObscured;
+    bool bottomObscured;
+    uint8_t  type;
+};
+
+struct VoxelModel {
+    Mesh _mesh;
+    uint_fast32_t vertCount;
+    uint_fast32_t texCount;
+};
+
+// Declarations
+Vector3 castVector3(int x, int y, int z);
+int SetVertex(VoxelModel* _model, uint16_t x, uint16_t y, uint16_t z, float tx1, float tx2);
+int addCubeMesh(VoxelModel* _model, uint16_t x, uint16_t y, uint16_t z, float length, float width, float height,float tx1, float tx2, BlockData _block);
+int addCubeType(VoxelModel* _model, int x, int y, int z, BLOCK_TYPES block, BlockData _data);
+Model GenMeshCustom(std::vector<BlockData> _blockList);
