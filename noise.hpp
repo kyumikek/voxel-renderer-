9f96c07a51e2458d33ba2b7147d5a3947ae12fd8
kyubek <69163151+kyumikek@users.noreply.github.com>
2024-06-04 11:15:03 +0200

    Delete noise.hpp

--- noise.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-float fade(float t);
-float lerp(float t, float a, float b);
-float grad(int hash, float x, float y, float z);
-float perlin(float x, float y, float z, int permutation_table[256]);
